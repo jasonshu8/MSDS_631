ef1aeeed9f2201f176e178bc2080f71ffe54c3fc
Jason <js@a.ki>
2019-02-13 18:22:55 -0800

    Lecture 4 material

---
+++ Lecture Notes/Week 4/people.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 16:53:25 2019
+
+@author: jasonshu
+"""
+
+
+
+class Person:
+    def __init__(self, first, last, major, birthdate):
+        return None
